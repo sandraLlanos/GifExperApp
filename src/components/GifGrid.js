7ceebcae0c57865d23a449497e90c0baf3fc252c
Sandra Llanos <sllanos@bancolombia.com.co>
2020-09-04 15:31:10 -0500

    mostrar titulos e imagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
+    const [images, setImages] = useState([]);
+    console.log(images);
+    // const [count, setCount] = useState(0);
+
+    // useEffect: nos permite ejecutar codigo de forma condicional
+    // el segundo argumento es una lista de dependencias
+    // si se envia vacio este solo se va a disparar una unica vez 
+    //en el momento que es renderizado por primera vez
+    useEffect(() => {
+        getGifts();       
+    },[])
 
     const getGifts = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=XRnsOEVygIHs5bH4hMpOall24oypnnmH'
@@ -8,19 +20,30 @@ export const GifGrid = ({category}) => {
         const {data} = await resp.json();
         console.log(data);
 
-        const gifs = data.map( ({id,title,url}) => {
+        const gifs = data.map( (img) => {
             return ({
-                id,
-                title,
-                url
+                id:img.id,
+                title:img.title,
+                url:img.images?.downsized_medium.url
             });
         });
         console.log(gifs);  
+        setImages(gifs);
     }
-    getGifts();
+    // getGifts(); se solucione la recarga del componente con useEffects
     return (
         <div>
             <h3>{category}</h3>
+            {/* <h3>{count}</h3>
+            <button onClick={()=>setCount(count + 1)}>1</button>         */}
+                {
+                    images.map( img => (
+                        <GifGridItem
+                        key={img.id}
+                        {...img}
+                        />
+                    ))
+                }
         </div>
     )
 }
